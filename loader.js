a5b0e17339084039b97620d1c92cae77c2d16a70
Paul Fillingham <146293896+dreamtargets-studio@users.noreply.github.com>
2026-02-15 22:23:42 +0000

    Added page top function

--- loader.js
+++ loader.js
@@ -12,3 +12,22 @@ window.addEventListener('DOMContentLoaded', () => {
     loadComponent('main-nav', 'header.html');
     loadComponent('main-footer', 'footer.html');
 });
+
+
+window.addEventListener('scroll', () => {
+    const btn = document.getElementById("backToTop");
+    if (btn) {
+        if (window.scrollY > 400) { // Appears after 400px of scrolling
+            btn.classList.add("show");
+        } else {
+            btn.classList.remove("show");
+        }
+    }
+});
+
+// The Jump-to-Top Logic
+document.addEventListener('click', (e) => {
+    if (e.target.closest('#backToTop')) {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+});
